5690e7bba93026578aedc955fe807883e03b49c7
Willian <willianjunior.c.f@gmail.com>
2025-11-18 19:52:06 -0300

    Fix: Voltar da pagina relatorio.

--- src/app/components/relatorio/relatorio.component.ts
+++ src/app/components/relatorio/relatorio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Button} from "primeng/button";
 import {FormsModule} from "@angular/forms";
 import {PrimeTemplate} from "primeng/api";
@@ -7,29 +7,45 @@ import {Toolbar} from "primeng/toolbar";
 import {Venda} from '../../model/vendaProdutos';
 import {Router} from '@angular/router';
 import {VendaService} from '../../service/venda.service';
+import {AuthService} from '../../service/auth.service';
+import {Usuario} from '../../model/usuario';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-relatorio',
-    imports: [
-        Button,
-        FormsModule,
-        PrimeTemplate,
-        TableModule,
-        Toolbar
-    ],
+  imports: [
+    Button,
+    FormsModule,
+    PrimeTemplate,
+    TableModule,
+    Toolbar,
+    NgIf
+  ],
   templateUrl: './relatorio.component.html',
+  standalone: true,
   styleUrl: './relatorio.component.scss'
 })
-export class RelatorioComponent {
+export class RelatorioComponent implements OnInit {
+
+  usuarioLogado!: Usuario;
+  vendas: Venda[] = [];
 
   constructor(private router: Router,
-              private vendaService: VendaService) {
+              private vendaService: VendaService,
+              private authService: AuthService) {}
+
+  ngOnInit(): void {
     if (typeof window !== 'undefined') {
       this.vendas = this.vendaService.getVendas();
     }
-  }
 
-  vendas: Venda[] = [];
+    // Escuta usuário logado
+    this.authService.usuarioInfo$.subscribe(u => {
+      if (u) {
+        this.usuarioLogado = u;
+      }
+    });
+  }
 
   protected voltar() {
     this.router.navigate(['/home']);
